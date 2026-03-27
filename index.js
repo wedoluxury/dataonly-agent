2c32fd1c51c5ffd755d21a88276d175872181f60
wedoluxury <124495058+wedoluxury@users.noreply.github.com>
2026-03-27 02:49:19 -0400

    Use environment variable for server port

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("🚀 Server running on port 3000");
 });
